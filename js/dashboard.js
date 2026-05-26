bc52152a9815bdd4cba40de5d01b64fa31d4b6df
Naveen142005 <valarmathi43679@gmail.com>
2026-05-27 00:35:07 +0530

    Page 3 done with media query. feels good

--- js/dashboard.js
+++ js/dashboard.js
@@ -42,7 +42,7 @@ new Chart(canva, {
                 grid: {
                     display: false
                 },
-              
+
                 ticks: {
                     color: "#8d87a8",
                     autoSkip: false,
@@ -82,6 +82,40 @@ new Chart(canva, {
                     display: false
                 }
             }
+        }
+    }
+});
+
+
+const sideBar = document.getElementById('side-bar');
+const main = document.getElementById('main');
+const menuIcon = document.getElementById('menu-icon')
+
+document.addEventListener('click', (e) => {
+    if (!sideBar.contains(e.target) && sideBar.classList.contains('active')) {
+        sideBar.classList.remove('active');
+        return;
+    }
+})
+
+menuIcon.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const isMobile = window.innerWidth <= 768;
+    console.log(isMobile);
+    if (isMobile) {
+        sideBar.classList.add('active')
+    }
+    else {
+        if (sideBar.classList.contains('closed')) {
+            sideBar.classList.remove('closed');
+            main.style.marginLeft = '245px';
+            main.style.width = 'calc(100vw - 245px)';
+            sideBar.style.left = '0px'
+        } else {
+            sideBar.classList.add('closed');
+            main.style.marginLeft = '0px';
+            main.style.width = '100vw';
+            sideBar.style.left = '-500px'
         }
     }
 });
