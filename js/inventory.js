0474e688e2d6f7d82a82bee90c22484fb52f5fc8
Naveen142005 <valarmathi43679@gmail.com>
2026-05-28 17:58:40 +0530

    Inventory list page completed fully...

---
+++ js/inventory.js
@@ -0,0 +1,52 @@
+const btn = document.getElementById('three-dot')
+const list = document.getElementById('list')
+
+btn.addEventListener('click', (e) => {
+    e.stopPropagation();
+    btn.classList.add('active-three-dot')
+    list.style.display=`flex`;
+})
+
+document.addEventListener('click', (e) => {
+    if (btn.classList.contains('active-three-dot') && !btn.contains(e.target) && !list.contains(e.target)){
+        btn.classList.remove('active-three-dot')
+        list.style.display='none'
+        return;
+    }
+})
+
+
+
+
+const sideBar = document.getElementById('side-bar');
+const main = document.getElementById('main');
+const menuIcon = document.getElementById('menu-icon')
+
+document.addEventListener('click', (e) => {
+    if (!sideBar.contains(e.target) && sideBar.classList.contains('active')) {
+        sideBar.classList.remove('active');
+        return;
+    }
+})
+
+menuIcon.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const isMobile = window.innerWidth <= 768;
+    console.log(isMobile);
+    if (isMobile) {
+        sideBar.classList.add('active')
+    }
+    else {  
+        if (sideBar.classList.contains('closed')) {
+            sideBar.classList.remove('closed');
+            main.style.marginLeft = '200px';
+            main.style.width = 'calc(100vw - 200px)';
+            sideBar.style.left = '0px'
+        } else {
+            sideBar.classList.add('closed');
+            main.style.marginLeft = '0px';
+            main.style.width = '100vw';
+            sideBar.style.left = '-500px'
+        }
+    }
+});
