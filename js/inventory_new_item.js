1075b8cb728fabbd3231c3aee605ef16fca6d1d7
Naveen142005 <valarmathi43679@gmail.com>
2026-05-28 23:10:37 +0530

    Inventory new item completed...

---
+++ js/inventory_new_item.js
@@ -0,0 +1,32 @@
+const sideBar = document.getElementById('side-bar');
+const main = document.getElementById('main');
+const menuIcon = document.getElementById('menu-icon')
+
+document.addEventListener('click', (e) => {
+    if (!sideBar.contains(e.target) && sideBar.classList.contains('active')) {
+        sideBar.classList.remove('active');
+        return;
+    }
+})
+
+menuIcon.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const isMobile = window.innerWidth <= 768;
+    console.log(isMobile);
+    if (isMobile) {
+        sideBar.classList.add('active')
+    }
+    else {  
+        if (sideBar.classList.contains('closed')) {
+            sideBar.classList.remove('closed');
+            main.style.marginLeft = '200px';
+            main.style.width = 'calc(100vw - 200px)';
+            sideBar.style.left = '0px'
+        } else {
+            sideBar.classList.add('closed');
+            main.style.marginLeft = '0px';
+            main.style.width = '100vw';
+            sideBar.style.left = '-500px'
+        }
+    }
+});
